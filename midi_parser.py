14bdb089e3f228fba703c041096b4779cf36af7a
loovjo <jonathan.loov@gmail.com>
2020-12-14 01:45:00 +0100

    stuff

--- midi_parser.py
+++ midi_parser.py
@@ -6,29 +6,36 @@ def midi_note_to_freq(note):
     # A4 = 69mid = 440Hz
     return 440 * 2 ** ((note - 69) / 12)
 
-def track_to_timestamps(track, channel, time_scale):
+def track_to_timestamps(track, channel, time_scale, use_min):
     t = 0
     out = [] # [(freq, time)]
+
+    current_note = -1
+
     for msg in track:
-        if msg.type == "note_on" and msg.channel == channel:
+        if msg.type == "note_on" and (msg.channel == channel or channel == -1):
             t += msg.time
 
             if msg.velocity > ON_THRESHOLD:
-                freq = midi_note_to_freq(msg.note)
+                if ((msg.note < current_note) != use_min) or current_note == -1:
+                    freq = midi_note_to_freq(msg.note)
+                    current_note = msg.note
+                else:
+                    continue
             else:
-                freq = 0
+                if msg.note == current_note:
+                    freq = 0
+                    current_note = -1
+                else:
+                    continue
 
             out.append((t * time_scale, freq))
 
     return out
 
-def read_midi(file_to_parse, track, channel):
-
-    file_to_parse = "MIDI-Samples/Jingle_Bells.mid"
-
+def read_midi(file_to_parse, track, channel, use_min, ts):
     mid = mido.MidiFile(file_to_parse)
 
     track = mid.tracks[track]
 
-
-    return track_to_timestamps(track, channel, 1 / mid.ticks_per_beat)
+    return track_to_timestamps(track, channel, (1 / ts) / mid.ticks_per_beat, use_min)
